9e19f16b9f9bb89b37a9e90cd989e9cb2ececefa
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-01-08 16:56:37 +0100

    foundation update

--- foundation/index.php
+++ foundation/index.php
@@ -1,31 +1,30 @@
-
 <!doctype html>
 <html class="no-js" lang="en" dir="ltr">
   <head>
-    <link rel="stylesheet" type="text/css" href="app.css">
 
+    <div class="grid-container">
+    <link rel="stylesheet" type="text/css" href="app.css">
     <?php include_once 'template\head.php'; ?>
+    <?php include_once 'template\titlebar.php'; ?>
+    </div>
 
   </head>
-<body>
 
-  <div class="grid-container">
-  <?php include_once 'template\titlebar.php'; ?>
-  </div>
+  <body>
 
-  <div class="grid-container" style= "padding: 5em;">
-    <div class="centered" style= "font-size: 3em;"> Dobrodošli na zimski zadatak </div>
-      <div class = "largeButton">
-        <a href="login.php" target="_blank"> Login </a>
-      </div>
-    <img src="../pictures/Pozadina.jpg" alt="Pozadina" width="1000px" height="1000px">
-  </div>
+    <div class="grid-container" style= "padding: 5em;">
+      <div class="centered" style= "font-size: 3em;"> Dobrodošli na zimski zadatak </div>
+        <div class = "largeButton">
+          <a href="login.php" target="_blank"> Login </a>
+        </div>
+      <img src="../pictures/Pozadina.jpg" alt="Pozadina" width="1000px" height="1000px">
+    </div>
 
-  <div class="grid-container">
-    <hr>
-      <?php include_once 'template\footer.php'; ?>
-  </div>
-      <?php include_once 'template\skripte.php'; ?>
-  
-</body>
+    <div class="grid-container">
+      <hr>
+        <?php include_once 'template\footer.php'; ?>
+    </div>
+        <?php include_once 'template\skripte.php'; ?>
+    
+  </body>
 </html>
--- foundation/nadzornaploca.php
+++ foundation/nadzornaploca.php
@@ -1,1 +1,28 @@
-1
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+
+    <div class="grid-container">
+    <link rel="stylesheet" type="text/css" href="app.css">
+    <?php include_once 'template\head.php'; ?>
+    <?php include_once 'template\titlebar.php'; ?>
+    </div>
+
+  </head>
+
+  
+  <body>
+  Login successful <?php echo $_GET["name"]; ?><br>
+
+
+    <div class= "backButton">
+        <a href="index.php">Back</a>
+    </div>
+
+    <div class="grid-container">
+      <hr>
+        <?php include_once 'template\footer.php'; ?>
+    </div>
+        <?php include_once 'template\skripte.php'; ?>
+  </body>
+</html>
