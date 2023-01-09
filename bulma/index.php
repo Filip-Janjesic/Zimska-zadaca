4f032006cf4e9851e7526f2e37ea46752dfa40dd
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-01-09 18:01:54 +0100

    bulma

---
+++ bulma/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bulma.min.css">
+    <link rel="stylesheet" href="css/app.css">
+    <script src='js/bootstrap.min.js'></script>
+    <?php include_once 'template\head.php'; ?>
+    <?php include_once 'template\navbar.php'; ?>
+</head>
+<body>
+
+
+<hr>
+<?php include_once 'template\footer.php'; ?>
+</body>
+</html>
---
+++ bulma/template/navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io" target="_blank">
+            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+        </a>
+    </div>
+    <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+            <a class="navbar-item" href="https://github.com/Filip-Janjesic/Zimska-zadaca" target="_blank"> 
+                Filip Janješić
+            </a>
+        </div>
+    </div>
+</nav>
